d6f630f52b923d3e335a188ac1833344488c95ca
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-12-03 14:10:23 -0300

    first commit

---
+++ LearnState/Home/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  LearnState
+//
+//  Created by Antonio Vuono on 03/12/22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
