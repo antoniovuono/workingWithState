d04554da11b2d504279368a5b6b10db9f09c3ac3
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-12-03 12:58:08 -0300

    Initial Commit

---
+++ LearnState/LearnStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnStateApp.swift
+//  LearnState
+//
+//  Created by Antonio Vuono on 03/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LearnStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
